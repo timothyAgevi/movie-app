02175fb50d18ca17462aa49327d1b2ba8325d759
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 21:01:53 +0300

    add movieCard

--- src/App.js
+++ src/App.js
@@ -39,21 +39,7 @@ const  App= () => {
             />
         </div>
         <div className="container">
-        <div className="movie">
-            <div>
-                <p>{movie1.Title} </p>
-            </div>
-            <div>
-                <img 
-                src={movie1.Poster !=='N/A'? movie1.Poster :'https://via.placeholder.com/400'} alt={movie1.Title}
-                />
-            </div>
-
-            <div>
-                <span> {movie1.Type}</span>
-                <h3> {movie1.Title}</h3>
-            </div>
-        </div>
+        
         </div>
         </div>
      );
