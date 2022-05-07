3e490108de03aaa34e1a5f783a1feb395f7886f9
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 13:23:46 +0300

    set counter

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 
 const App=()=> {
   //hook: anything called as a function begining with use
+  //inside [nameod State,setterfunction]=useState(initialvalue) 
+  const [counter,setCounter]=useState(0);
   return (
     <div className="App">
      <button> -</button>
-     <h1> 0</h1>
+     <h1> {counter}</h1>
      <button> +</button>
     </div>
   );
