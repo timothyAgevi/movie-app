f49e72769f754d04becfbe411e4c8b91d327f45b
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 13:19:35 +0300

    add hook defination

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 const App=()=> {
+  //hook: anything called as a function begining with use
   return (
     <div className="App">
      <button> -</button>
