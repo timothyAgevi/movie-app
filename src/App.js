4d9a8c7c6b4584410ac88a07287ef64956ae6d60
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 15:26:34 +0300

    add search svg

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ const API_URL=' http://www.omdbapi.com/?i=tt3896198&apikey=16074f9e'
 
 const  App= () => {
     const searchMovies= async(title)=>{
-        const response=await fetch(`${API_URL} & s=${title}`)
+        const response=await fetch(`${API_URL}&s=${title}`)
         const data= await response.json();
-        console.log(data);
+        console.log(data.Search);
     }
     useEffect(()=>{
-
+       searchMovies('Spiderman');
     },[]);
 
     return ( 
