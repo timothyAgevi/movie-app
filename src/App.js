773b31b8e3c5575c8fb5cba275446c3633c95d5f
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 14:50:28 +0300

    add

--- src/App.js
+++ src/App.js
@@ -3,10 +3,15 @@ import {useEffect} from "react";
 const API_URL=' http://www.omdbapi.com/?i=tt3896198&apikey=16074f9e'
 
 const  App= () => {
+    const searchMovies= async(title)=>{
+        const response=await fetch(`${API_URL} & s=${title}`)
+        const data= await response.json();
+        console.log(data);
+    }
     useEffect(()=>{
 
     },[]);
-    
+
     return ( 
         <h1>App</h1>
      );
