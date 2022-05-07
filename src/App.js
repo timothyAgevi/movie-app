10c9bc57bb1084e46054c4e189c893dce8fc86cf
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 15:37:58 +0300

    add movieland serachbar and title

--- src/App.js
+++ src/App.js
@@ -16,7 +16,13 @@ const  App= () => {
     },[]);
 
     return ( 
-        <h1>App</h1>
+        <div className="app">
+        <h1>MovieLand</h1>
+        <div className="search">
+            <input
+            placeholder="Search for movies"/>
+        </div>
+        </div>
      );
 }
  
