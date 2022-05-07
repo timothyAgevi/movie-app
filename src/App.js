f858a4bf4bac8173e97d6f61a6e4239016a3899e
timothyAgevi <timothyagevi@gmail.com>
2022-05-07 13:30:37 +0300

    add calback prevCount to events

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ const App=()=> {
   const [counter,setCounter]=useState(0);
   return (
     <div className="App">
-     <button onClick ={()=>setCounter(-5)}> -</button>
+     <button onClick ={()=>setCounter((prevCount)=>prevCount -1)}> -</button>
      <h1> {counter}</h1>
-     <button onClick={()=>setCounter(+5)}> +</button>
+     <button onClick={()=>setCounter( (prevCount)=>prevCount + 1)}> +</button>
     </div>
   );
 }
