9eea05c7f6b26df068e3d2c600b0be3b15479cbe
timothyAgevi <timothyagevi@gmail.com>
2022-05-08 21:33:59 +0300

    map  over all movies

--- src/App.js
+++ src/App.js
@@ -41,9 +41,21 @@ const  App= () => {
             alt="search"
             />
         </div>
-        <div className="container">
-        <MovieCard movie1={movies[0]}/>
-        </div>
+        {
+            movies?.length>0?(
+                <div className="container">
+                {movies.map((movie)=>(
+                    <MovieCard movie={movie}/>
+                ))}
+                </div>   
+            ):
+            (
+                <div className="empty">
+                    <h2> No movies found</h2>
+                    </div>
+            )
+        }
+       
         </div>
      );
 }
